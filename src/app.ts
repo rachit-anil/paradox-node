ee06bc66d1d1b0fc70393790e5c172b98080b7ec
rachit-anil <rachit9910102312@gmail.com>
2025-02-28 20:40:18 +0530

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Custom CORS middleware
+app.use(
+  cors({
+    origin: "http://localhost:4210", // Allow only this origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow only these HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow only these headers
+    credentials: true, // Allow cookies and credentials to be sent
+  })
+);
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // Allow specific origin
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific methods
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+//   res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+//   next();
+// });
+
+const port = process.env.PORT;
+const FAKE_DATA = {
+  message: "A message from node",
+};
+
+app.get("/auth/login", (req, res) => {
+  res.send(FAKE_DATA);
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
