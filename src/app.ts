d1d07adf6c9656c0f2bc94e61461a540ef670917
rachit-anil <rachit9910102312@gmail.com>
2025-02-28 20:54:59 +0530

    env and cors

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,10 @@ dotenv.config();
 const app = express();
 
 // Custom CORS middleware
+const allowedOrigins = ["http://localhost:4200", "http://projectparadox.in"];
 app.use(
   cors({
-    origin: "http://localhost:4210", // Allow only this origin
+    origin: allowedOrigins, // Allow only this origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow only these HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow only these headers
     credentials: true, // Allow cookies and credentials to be sent
